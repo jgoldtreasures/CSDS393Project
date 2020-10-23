521b557417821aab9e8a32738f903b9da1a3cd49
Julian Robinson <jer171@case.edu>
2020-10-23 18:48:07 -0400

    Added getTasks and getNpcs instead of the getInteractables method. Added removeTask and changeNpc instead of the changeInteractable method. Many errors due to dependency issues

--- idk/src/Building.java
+++ idk/src/Building.java
@@ -1,6 +1,57 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Building {
-    int x;
-    int y;
+    private int x;
+    private int y;
+    private TaskManager tasks;
+    private Person[] npcs;
 
+    public Building(int x, int y, TaskManager tasks, Person[] npcs) {
+        this.x = x;
+        this.y = y;
+        this.tasks = tasks;
+        this.npcs = npcs;
+    }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setPos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public TaskManager getTasks() {
+        return this.tasks;
+    }
+
+    public void removeTask(String task) {
+        List<Task> taskList = Arrays.asList(tasks.getTasks());
+        taskList.removeIf(task1 -> task1.getName() == task);
+        tasks = new TaskManager(taskList);      //need task manager constructor to take list of tasks as argument
+    }
+
+    public Person[] getNpcs() {
+        return this.npcs;
+    }
+
+    public Boolean changeNpc(String name) {
+        List<Person> npcList = Arrays.asList(npcs);
+        boolean changed = false;
+        for (Person npc : npcList) {
+            if (npc.getName == name) {
+                npcList.remove(npc);
+                npc.setStatus("DORMANT");      //need npc to have setStatus method
+                changed = true;
+                npcList.add(npc);
+            }
+        }
+        return changed;
+    }
 }
