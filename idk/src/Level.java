90a56cc4441f521a8d2ab3a9fd38d8de146ac452
athomasx <51031785+athomasx@users.noreply.github.com>
2020-10-23 19:58:55 -0400

    put classes in seperate files

--- idk/src/Level.java
+++ idk/src/Level.java
@@ -1,2 +1,20 @@
 public class Level {
+    private ArrayList(Task) tasks;
+
+    public Level(){
+        tasks = new ArrayList(Task);
+    }
+
+    public void addTask(int numActions){
+        tasks.add(new Task(numActions));
+    }
+
+    public boolean isFinished(){
+        for(int i = 0; i < actions.length; i++){
+            if(actions[i].isFinished() == false){
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- idk/src/Task.java
+++ idk/src/Task.java
@@ -1,2 +1,23 @@
 public class Task {
+    private boolean[] actions;
+
+    public Task(int numActions){
+        actions = new boolean[numActions];
+        for (int i = 0; i < numActions; i++){
+            actions[i] = false;
+        }
+    }
+
+    public void completeAction(int index){
+        actions[index] = true;
+    }
+
+    public boolean isFinished(){
+        for(int i = 0; i < actions.length; i++){
+            if(actions[i] == false){
+                return false;
+            }
+        }
+        return true;
+    }
 }
