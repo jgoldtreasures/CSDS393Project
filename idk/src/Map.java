1efc2ae30befd151cf294575e15facc6793ecdb8
Julian Robinson <jer171@case.edu>
2020-10-25 00:07:01 -0400

    I really hope I dont fuck over the entire repo with this push lol

--- idk/src/Map.java
+++ idk/src/Map.java
@@ -1,3 +1,15 @@
 public class Map {
+    private Building[] buildings;
 
+    public Map(Building[] buildings){
+        this.buildings = buildings;
+    }
+
+    public Building[] getBuildings() {
+        return buildings;
+    }
+
+    public void enterBuilding(){
+
+    }
 }
--- idk/src/TaskManager.java
+++ idk/src/TaskManager.java
@@ -1,2 +1,6 @@
 public class TaskManager {
+
+    public TaskManager(){
+
+    }
 }
