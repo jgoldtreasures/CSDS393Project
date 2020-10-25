4001b1038c7a4de69fdc58ed5d3d723619e91713
lizshu <elshumake@gmail.com>
2020-10-24 23:32:42 -0500

    Fixed some issues with test layout, there is an addition to Game project that'll need to be changed

--- test/PlayerTests.java
+++ test/PlayerTests.java
@@ -4,17 +4,36 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PlayerTests {
-    static int xVal = 50;
-    static int yVal = 50;
-    public static Player p1 = new Player(xVal, yVal);
+    int xVal = 50;
+    int yVal = 50;
+    public Player p1 = new Player(xVal, yVal);
 
     @Test
-    public void getXTest(){
+    public void getPosTests(){
         assertEquals(p1.getX(),xVal);
-    }
-
-    @Test
-    public void getYTest(){
         assertEquals(p1.getY(),yVal);
     }
+
+    @Test
+    public void setPosTests(){
+        int newX = 60;
+        int newY = 75;
+        p1.setX(newX);
+        p1.setY(newY);
+        assertEquals(p1.getX(), newX);
+        assertEquals(p1.getY(), newY);
+    }
+
+   /* @Test
+    public void setStatusTest(){
+
+    }
+
+    /*
+    @Test
+    public void setYTest(){
+
+    }
+    */
+
 }
