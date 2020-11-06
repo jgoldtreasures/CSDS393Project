a45e0fe86ac827e384e542be970363cbe28a62f0
athomasx <51031785+athomasx@users.noreply.github.com>
2020-11-06 17:52:36 -0500

    fixed gameTest

--- idk/src/Game.java
+++ idk/src/Game.java
@@ -19,9 +19,20 @@ public class Game{
         m = new Map(b);
 
         Freshman = new Level();
+        Freshman.addTask("Choose Roommate", "Go to the Freshman Dorms and choose one of the available roommates", 2, new int[]{1,0,0,0,0});
+        Freshman.addTask("Take Campus Tour", "Visit every building on campus", 10, new int[]{0, 1, 0, 0, 0});
+
         Sophomore = new Level();
+        Sophomore.addTask("Declare Major", "Go to Nord Hall and fill out a declare major form", 2, new int[]{1,0,0,1,1,0});
+        Sophomore.addTask("Join Club", "Go to Tinkham Veale University Center and join one of the available clubs", 2, new int[]{0,0,1,0,1});
+
         Junior = new Level();
+        Junior.addTask("Pass test", "Go to the Bingham building, take the necessary test, and pass it", 3, new int[]{0,0,1,1,0,0});
+        Junior.addTask("Study", "Go to the Kelvin Smith Library and study one of the books", 2, new int[]{0,0,0,1,1});
+
         Senior = new Level();
+        Senior.addTask("Take Gym", "Go to the Veale Gym and take one of the available gym classes", 2, new int[]{0,0,1,1,0,0});
+        Senior.addTask("Complete Capstone", "Go to you major's specific buildings and complete your major's specific objectives to graduate!", 6, new int[]{2,2,2,2,2});
 
         load();
 
@@ -36,6 +47,7 @@ public class Game{
 
     public boolean gameFinished() {
         if (Senior.isFinished()) {
+            System.out.println("Congratulaitons! You Win!");
             return true;
         }
         return false;
@@ -85,37 +97,19 @@ public class Game{
     public void load(){
         Scanner reader = new Scanner("save.txt");
 
-        if(reader.hasNext() && reader.nextLine().equals("false")){
-            Freshman.addTask("Choose Roommate", "Go to the Freshman Dorms and choose one of the available roommates", 2, new int[]{1,0,0,0,0});
-            Freshman.addTask("Take Campus Tour", "Visit every building on campus", 10, new int[]{0, 1, 0, 0, 0});
-        }
-        else{
+        if(reader.hasNext() && reader.nextLine().equals("true")){
             Freshman.setFinish();
         }
 
-        if(reader.hasNext() && reader.nextLine().equals("false")){
-            Sophomore.addTask("Declare Major", "Go to Nord Hall and fill out a declare major form", 2, new int[]{1,0,0,1,1,0});
-            Sophomore.addTask("Join Club", "Go to Tinkham Veale University Center and join one of the available clubs", 2, new int[]{0,0,1,0,1});
-
-        }
-        else{
+        if(reader.hasNext() && reader.nextLine().equals("true")){
             Sophomore.setFinish();
         }
 
-        if(reader.hasNext() && reader.nextLine().equals("false")){
-            Junior.addTask("Pass test", "Go to the Bingham building, take the necessary test, and pass it", 3, new int[]{0,0,1,1,0,0});
-            Junior.addTask("Study", "Go to the Kelvin Smith Library and study one of the books", 2, new int[]{0,0,0,1,1});
-        }
-        else{
+        if(reader.hasNext() && reader.nextLine().equals("true")){
             Junior.setFinish();
         }
 
-        if(reader.hasNext() && reader.nextLine().equals("false")){
-            Senior.addTask("Take Gym", "Go to the Veale Gym and take one of the available gym classes", 2, new int[]{0,0,1,1,0,0});
-            Senior.addTask("Complete Capstone", "Go to you major's specific buildings and complete your major's specific objectives to graduate!", 6, new int[]{2,2,2,2,2});
-
-        }
-        else{
+        if(reader.hasNext() && reader.nextLine().equals("true")){
             Senior.setFinish();
         }
     }
