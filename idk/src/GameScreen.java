9f4f023972df0163ab03cfb61d85bce046a83562
lizshu <elshumake@gmail.com>
2020-11-16 19:22:31 -0600

    changes default screen to the quad map

--- idk/src/GameScreen.java
+++ idk/src/GameScreen.java
@@ -1,16 +1,21 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import javax.swing.*;
 
 public class GameScreen {
     JFrame gameScreen;
     Container con;
-    JPanel titleNamePanel, startButtonPanel, loadButtonPanel;
-    JLabel titleNameLabel;
+    JPanel titleNamePanel, startButtonPanel, loadButtonPanel, imagePanel;
+    JLabel titleNameLabel, imageLabel;
     JButton startButton, loadButton;
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 80);
     Font normalFont = new Font("Times New Roman", Font.PLAIN, 25);
-    private JButton button1;
-    private JPanel panel1;
+
+    ImageIcon image;
+
+    ScreenHandler sHandler = new ScreenHandler();
 
     public static void main(String[] args){
         new GameScreen();
@@ -40,12 +45,16 @@ public class GameScreen {
         startButton.setForeground(Color.white);
         startButton.setFont(normalFont);
 
+        startButton.addActionListener(sHandler);
+
         loadButtonPanel = new JPanel();
         loadButtonPanel.setBounds(300, 350 ,150, 50);
         loadButton = new JButton("Load");
         loadButton.setBackground(Color.darkGray);
         loadButton.setForeground(Color.white);
         loadButton.setFont(normalFont);
+
+        loadButton.addActionListener(sHandler);
 
         titleNamePanel.add(titleNameLabel);
         startButtonPanel.add(startButton);
@@ -59,10 +68,70 @@ public class GameScreen {
 
         //popup menu window
 
-        //image for map
+        //image for maps
+        imagePanel = new JPanel();
+        imagePanel.setBounds(-10,-10,800,600);
+        imageLabel = new JLabel();
+
+
+        image = new ImageIcon("idk/resources/CWRUquadDots.jpg");
+        Image resizedImage = getScaledImage(image.getImage(), 800, 600);
+        image.setImage(resizedImage);
+
+        imageLabel.setIcon(image);
+        imagePanel.add(imageLabel);
 
         //image for each building
         //Will take a specfic image from a connected folder for the image
         //will also standardize the format for the buildings
+    }
+
+    public void createQuadScreen(){
+        titleNamePanel.setVisible(false);
+        startButtonPanel.setVisible(false);
+        loadButtonPanel.setVisible(false);
+
+        con.add(imagePanel);
+        imagePanel.setVisible(true);
+
+        //player object/image is implemented and can be moved around
+        //if at location to change maps, do so
+        //if building thing
+    }
+
+    public void createNorthScreen(){
+        image = new ImageIcon("idk/resources/CWRUnorthDots.jpg");
+        Image resizedImage = getScaledImage(image.getImage(), 800, 600);
+        image.setImage(resizedImage);
+        imageLabel.setIcon(image);
+    }
+
+    public void createSouthScreen(){
+        image = new ImageIcon("idk/resources/CWRUsouthDots.jpg");
+        Image resizedImage = getScaledImage(image.getImage(), 800, 600);
+        image.setImage(resizedImage);
+        imageLabel.setIcon(image);
+    }
+
+    public void createBuildingScreen(){
+
+    }
+
+    private Image getScaledImage(Image srcImg, int w, int h){
+        BufferedImage resizedImage = new BufferedImage(w,h,BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = resizedImage.createGraphics();
+
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(srcImg, 0, 0, w, h, null);
+        g2.dispose();
+
+        return resizedImage;
+    }
+
+    public class ScreenHandler implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+            //Need to add a manner which differentiates between start/load
+            createQuadScreen();
+        }
     }
 }
