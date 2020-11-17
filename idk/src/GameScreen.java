07b867507dea9e44665f04f6bfe8aa3c1c2e0f74
lizshu <elshumake@gmail.com>
2020-11-16 20:13:56 -0600

    added more functionality to screen

--- idk/src/GameScreen.java
+++ idk/src/GameScreen.java
@@ -5,13 +5,17 @@ import java.awt.image.BufferedImage;
 import javax.swing.*;
 
 public class GameScreen {
+    public String screenName;
+
     JFrame gameScreen;
     Container con;
-    JPanel titleNamePanel, startButtonPanel, loadButtonPanel, imagePanel;
+    JPanel titleNamePanel, startButtonPanel, loadButtonPanel, imagePanel,introTextPanel;
     JLabel titleNameLabel, imageLabel;
     JButton startButton, loadButton;
+    JTextArea introTextArea;
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 80);
     Font normalFont = new Font("Times New Roman", Font.PLAIN, 25);
+    Font introFont = new Font("Times New Roman", Font.PLAIN, 20);
 
     ImageIcon image;
 
@@ -22,11 +26,12 @@ public class GameScreen {
     }
 
     public GameScreen() {
-        //image homescreen - intro, save, and load
+        //image home screen - intro, save, and load
+        screenName = "Title Screen";
+
         gameScreen = new JFrame();
         gameScreen.setSize(800,600);
         gameScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //gameScreen.getContentPane().setBackground(Color.BLACK);
         gameScreen.setLayout(null);
         con = gameScreen.getContentPane();
 
@@ -86,10 +91,41 @@ public class GameScreen {
         //will also standardize the format for the buildings
     }
 
-    public void createQuadScreen(){
+    public void createIntroScreen(){
         titleNamePanel.setVisible(false);
-        startButtonPanel.setVisible(false);
         loadButtonPanel.setVisible(false);
+
+        screenName = "Intro Screen";
+        startButtonPanel.setBounds(300, 425, 150,50);
+
+        String textIntro ="You are a student at Case Western Reserve University. Your goal is to graduate from the " +
+                "university. There will be both required and optional tasks, but the required tasks will be within " +
+                "your task list at the start of each year. Every \"year\" will last 7 in-game days. \n\n" +
+                "When on one of the three maps, you may move around the screen using the arrow-keys and you can enter" +
+                " a nearby building by pressing 'enter'\n\n" +
+                "When in a building, you can cycle between interactable objects using the 'up' and 'down' arrow keys." +
+                " Pressing 'enter' will start an interaction with the object.\n\n" +
+                "On any screen, pressing 'm' will bring up the in-game menu, which will allow you to save the game, " +
+                "look at your attributes, look at your task list, and exit the game. \n\n" +
+                "Press 'Start' to begin the game. Have fun playing!";
+
+        introTextPanel = new JPanel();
+        introTextPanel.setBounds(25,50,725,375);
+        con.add(introTextPanel);
+
+        //find a way to center text
+        introTextArea = new JTextArea(textIntro);
+        introTextArea.setBounds(25,25,725,375);
+        introTextArea.setFont(introFont);
+        introTextArea.setLineWrap(true);
+        introTextArea.setWrapStyleWord(true);
+
+        introTextPanel.add(introTextArea);
+    }
+
+    public void createQuadScreen(){
+        startButtonPanel.setVisible(false);
+        introTextPanel.setVisible(false);
 
         con.add(imagePanel);
         imagePanel.setVisible(true);
@@ -131,7 +167,14 @@ public class GameScreen {
     public class ScreenHandler implements ActionListener{
         public void actionPerformed(ActionEvent event){
             //Need to add a manner which differentiates between start/load
-            createQuadScreen();
+            switch (screenName){
+                case "Title Screen":
+                    createIntroScreen();
+                    break;
+                case "Intro Screen":
+                    createQuadScreen();
+                    break;
+            }
         }
     }
 }
