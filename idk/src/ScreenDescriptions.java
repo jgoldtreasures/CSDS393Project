5d562d6d3b8d2c50e6bf8429ba369b31b93af315
athomasx <51031785+athomasx@users.noreply.github.com>
2020-12-03 22:06:03 -0500

    finished building descriptions

--- idk/src/ScreenDescriptions.java
+++ idk/src/ScreenDescriptions.java
@@ -14,20 +14,23 @@ public class ScreenDescriptions{
     public void buildingDescription(String buildingName) {
         switch (buildingName) {
             case ("Bingham"): //civil and environmental engineering, sleeping/studying
-                description = "Sleep";
+                description = "Contains the civil and environmental engineering offices, " +
+                        "lecture halls, and sleeping/studying spaces";
                 break;
             case ("Carlton"):
-                description = "";
+                description = "Carlton Commons has a few study spaces as well as the rough " +
+                        "rider room restaurant";
                 break;
             case ("Crawford"): //Access Services, SAGES office, a cafe, childhood,
                 // social justice institute,cognitive science studies,
-                description = "";
+                description = "Contains Access Services, SAGES office, a cafe, childhood, " +
+                        "social justice institute, cognitive science studies, and assorted classrooms";
                 break;
             case ("Dennys"):
-                description = "";
+                description = "A restaurant that allows CWRU special swipes and case cash";
                 break;
             case ("Eldred"): //department of theater
-                description = "";
+                description = "Houses the theater and theater department";
                 break;
             case ("Freshman Dorms"):
                 description = "Includes buildings such as Taplin, Taft, Smith, " +
@@ -51,56 +54,77 @@ public class ScreenDescriptions{
                         "rentable electronic devices.";
                 break;
             case ("KHS"): //macromolecular science and engineering
-                description = "";
+                description = "macromolecular science and engineering offices, as well as" +
+                        "lecture halls and labs";
                 break;
             case ("Leutner"):
-                description = "";
+                description = "One of the two main dining halls on campus, also contains " +
+                        "a restaurant and study spaces in the basement";
                 break;
             case ("Millis"): //Connected to Clapp where the department of Chemistry is
-                description = "";
+                description = "Contains Millis-Schmidt Auditorium, and is connected to " +
+                        "Clapp hall, home of the department of chemistry";
                 break;
             case ("Nord"): //computer lab, engineering leadership and professional
                 // practice, general engineering, peer advisors
-                description = "";
+                description = "Nord hall has computer labs, engineering leadership and professional " +
+                        "practice offices, general engineering office, peer advisor centers, " +
+                        "many lecture halls, study spaces, and Einstein's Bagels restaurant";
                 break;
             case ("Olin"):
-                description = "";
+                description = "Olin contains the EECS lab, lecture halls, and the EECS office";
                 break;
             case ("PBL"):
                 description = "All business related majors can be found within this " +
                         "building.";
                 break;
             case ("Rockefeller"): //physics, evolutionary biology
-                description = "";
+                description = "Contains the physics and evolutionary biology departments" +
+                        " as well as a few lecture halls and labs";
                 break;
             case ("Sears"): //multicultural affairs, career center, disability resources,
                 // educational services(mentoring/tutoring), study center, SOURCE
                 // office, counseling services, department of astronomy
-                description = "";
+                description = "The Sears building contains the office of multicultural affairs, the career center, " +
+                        " disability resources, educational services(mentoring/tutoring)," +
+                        " study centers, the SOURCE office, counseling services, and the" +
+                        " department of astronomy";
                 break;
             case ("Smith"): //department of chemical and biomolecular engineering,
                 // environmental studies program, earth environmental and planetary
                 // sciences, natural sciences
-                description = "";
+                description = "Smith hall contains the department of chemical and " +
+                        "biomolecular engineering, the environmental studies program," +
+                        " and the environmental and planetary sciences, and natural " +
+                        "sciences departments";
                 break;
             case ("Sophomore Dorms"):
-                description = "";
+                description = "Includes buildings such as Staley, Alumni, Tippit, " +
+                        "Glaser, Kusch, and Michelson dorms";
                 break;
             case ("Strosacker"): //largest lecture hall on campus AND it has a room
                 // upstiars where you can sit an study
-                description = "";
+                description = "Strosacker is the largest lecture hall on campus, and" +
+                        "contains study spaces as well";
                 break;
             case ("Thwing"): //postal substation, residence life, university housing,
                 // jolly scholar
-                description = "";
+                description = "Thwing has classrooms as well as the postal substation," +
+                        " residence life office, university housing, grabit restaurant," +
+                        " study spaces, and the jolly scholar bar";
                 break;
             case ("Tink"): //public sevice, commuter services, women's center, greek
                 // life, lgbt, student activities and leadership,
-                description = "";
+                description = "The Tinkham Veale University Center has the public service, " +
+                        " commuter services, women's center, greek " +
+                        "life, lgbt, student activities and leadership offices, " +
+                        "as well as multiple restaurants, study spaces, and event areas";
                 break;
             case ("Tomlinson"): //international affairs, international student
                 // services, religious studies
-                description = "";
+                description = "Tomlinson has the international affairs, international student" +
+                        " services, and religious studies offices, as well as a food court " +
+                        "in the basement";
                 break;
             case ("Veale"):
                 description = "This is CWRU's main GYM. This is the main location for " +
@@ -111,14 +135,18 @@ public class ScreenDescriptions{
                         "location, ThinkBox.";
                 break;
             case ("White"): //materials science and engineering
-                description = "";
+                description = "White building is home to the materials science and engineering" +
+                        "department, as well as a few lecture halls and classrooms";
                 break;
             case ("Wickenden"): //biomedical engineering
-                description = "";
+                description = "Wickenden has the biomedical engineering offices, along" +
+                        " with a few lecture halls";
                 break;
             case ("Yost"): //financial aid, student employment, student financial
                 // services, mathematics and statistics department
-                description = "";
+                description = "Yost houses the financial aid, student employment, " +
+                        "student financial services, offices, as well as the mathematics " +
+                        "and statistics department";
                 break;
             default:
                 description = "";
